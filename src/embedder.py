c37b5dbcaa1f709a57b456858518dfb1ac17323e
Shamil <shamilamiyan@gmail.com>
2026-03-13 05:20:09 +0530

    Refactor Embedder methods for consistency and add test script for document embedding

--- src/embedder.py
+++ src/embedder.py
@@ -4,10 +4,10 @@ class Embedder:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
         
-    def eccodeDocument(self, document):
-        embedings = self.model.encode(document)
-        return embedings
+    def encode_documents(self, document):
+        embeddings  = self.model.encode(document)
+        return embeddings 
     
     def encode_query(self, query):
-        embedings = self.model.encode(query)
-        return embedings
+        embeddings  = self.model.encode(query)
+        return embeddings 
